0876d9501a702f66c076a6f8730982afa1aaa43a
sashakluchnik <sasha@human-nyc.com>
2024-02-19 12:58:25 -0500

    added fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: [
+    '@/assets/css/fonts.css',
+    '@/assets/css/main.css',
+  ],
+  modules: [
+    // '@nuxtjs/sanity',
+    '@nuxtjs/tailwindcss',
+    // '@pinia/nuxt'
+  ],
   postcss: {
     plugins: {
       "postcss-simple-vars": {},
     },
   },
-  modules: ["@nuxtjs/tailwindcss"],
+  // sanity: {
+  //   projectId: ''
+  // },
 })
